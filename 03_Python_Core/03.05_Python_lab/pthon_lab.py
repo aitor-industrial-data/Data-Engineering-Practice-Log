b0fa712992836022a12dbe5459df0dfc90a14c96
aitor-industrial-data <aitor166@hotmail.com>
2026-08-11 12:51:57 +0200

    Update20260811

--- 03_Python_Core/03.05_Python_lab/pthon_lab.py
+++ 03_Python_Core/03.05_Python_lab/pthon_lab.py
@@ -8,10 +8,6 @@ ventas = pd.read_csv("ventas.csv")
 #calcular cuanto suma la columa de unit_price por quantity
 ventas["total"] = ventas["unit_price"] * ventas["quantity"]
 
-
-
-#sumar la columna total
-total_ventas = ventas["total"].sum()
-
-#eliminar filas con product_category nulo
-ventas = ventas.dropna(subset=["product_category"])
+# Si total > 1000 pone 'VIP', si no pone 'NORMAL'
+ventas["status"] = ["VIP" if total > 1000 else "NORMAL" for total in ventas["total"]]
+print(ventas)
